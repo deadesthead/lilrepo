fb4b5a7c7a4b71abb3e7d6cde90b11dbdf5bdcf3
deadesthead <webtm@openmailbox.org>
2016-07-14 13:09:41 -0400

    added

---
+++ config/openbox/autostart.sh
@@ -0,0 +1,14 @@
+tint2 &
+mate-power-manager &
+volumeicon &
+#mate-volume-control-applet &
+#nm-applet &
+#feh --bg-scale /home/web/Pictures/Anime/Nekomimi/cutegreypink2.jpg &
+#feh --bg-scale /home/web/Pictures/Anime/SolidColor/1416662898444.png &
+#xrandr --output VGA1 --auto --left-of LVDS1 --auto
+feh --bg-scale /home/web/pictures/cover_photos/bands/dead/tiedyedead.jpg &
+
+#feh --bg-scale /home/web/Pictures/Anime/SolidColor/1414809661004.png &
+#conky &
+#xscreensaver &
+#xpad &
